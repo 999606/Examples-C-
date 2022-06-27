5cf1157c1673899f7fe2bac377e4bd8c25b29aec
999606 <999606@mail.ru>
2022-06-27 13:37:45 +0300

    лекция 2 по массивам

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 81 , 72 , 63 , 4 , 54 , 36 , 27 , 81 };
+
+int n = array.Length;
+int find = 81;
+
+int index = 0;
+
+while (index < n )
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index +1;
+    index++;
+}
